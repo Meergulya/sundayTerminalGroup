0501a04fe17f5adbe0b496aa27d9bb8287b551ab
Meergul <mirgulyar@gmail.com>
2019-11-10 14:17:17 -0600

    first push

---
+++ src/terminalSunday/firstSundayTerminal.java
@@ -0,0 +1,20 @@
+package terminalSunday;
+
+
+
+public class firstSundayTerminal {
+
+    public static void main(String[] args) {
+        System.out.println(" Brand New Project");
+    }
+
+
+
+
+
+
+
+
+
+
+}
