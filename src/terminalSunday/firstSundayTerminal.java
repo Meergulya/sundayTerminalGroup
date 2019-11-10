92a7b2aa87f46fd6fc1f793a427f8dc5c1cad822
Meergul <mirgulyar@gmail.com>
2019-11-10 15:07:05 -0600

    recently changed

--- src/terminalSunday/firstSundayTerminal.java
+++ src/terminalSunday/firstSundayTerminal.java
@@ -6,6 +6,7 @@ public class firstSundayTerminal {
 
     public static void main(String[] args) {
         System.out.println(" Brand New Project");
+        System.out.println(" I added something");
     }
 
 
